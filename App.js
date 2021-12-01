5b1fcd2e0b553a67e83961e4bbaaeed6d21a8ecd
mystere974 <m.loghann@gmail.com>
2021-12-01 11:29:40 +0100

    first test success

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
+import HomeScreen from './screens/HomeScreen';
+import SettingScreen from './screens/SettingsScreen';
+import Tabs from './navigation/tabs';
+
+const Tab = createBottomTabNavigator();
+
+function app() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Tabs />
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default app;
